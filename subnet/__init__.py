f6670e3e1ddc1a53a08a287cc85667cec791465e
Jared McKnight <jmcknight@archertech.ca>
2025-06-23 14:40:51 -0400

    format code and add type hints

--- subnet/__init__.py
+++ subnet/__init__.py
@@ -1,15 +1,11 @@
-
 # Import everything from builtin module, so this can be used as a complete drop-in replacement
 from ipaddress import (
     IPv4Address,
     IPv6Address,
     IPv4Interface,
     IPv6Interface,
-    IPv4Network as builtin_IPv4Network,
-    IPv6Network as builtin_IPv6Network,
     ip_address,
     ip_interface,
-    ip_network as builtin_ip_network,
     collapse_addresses,
     get_mixed_type_key,
     summarize_address_range,
@@ -23,20 +19,25 @@ from .utils import (
     IPv4Network,
     IPv6Network,
     SubnetUtilsMixin,
+    ip_network,
 )
 
-
-def ip_network(*args, **kwargs):
-
-    net = builtin_ip_network(*args, **kwargs)
-
-    if isinstance(net, builtin_IPv4Network):
-        net.__class__ = IPv4Network
-
-    elif isinstance(net, builtin_IPv6Network):
-        net.__class__ = IPv6Network
-
-    else:
-        raise ValueError('Unhandled network type: %s' % type(net))
-
-    return net
+__all__ = [
+    "AddressValueError",
+    "IPv4Address",
+    "IPv6Address",
+    "IPv4Interface",
+    "IPv6Interface",
+    "IPv4Network",
+    "IPv6Network",
+    "NetmaskValueError",
+    "SubnetUtilsMixin",
+    "ip_address",
+    "ip_interface",
+    "ip_network",
+    "collapse_addresses",
+    "get_mixed_type_key",
+    "summarize_address_range",
+    "v4_int_to_packed",
+    "v6_int_to_packed",
+]
--- subnet/utils.py
+++ subnet/utils.py
@@ -1,8 +1,12 @@
-
+from __future__ import annotations
+import typing as t
 from ipaddress import (
+    IPv4Address,
+    IPv6Address,
     IPv4Network as builtin_IPv4Network,
     IPv6Network as builtin_IPv6Network,
     ip_address as builtin_ip_address,
+    ip_network as builtin_ip_network,
 )
 import math
 
@@ -10,9 +14,14 @@ from random import randint
 
 
 class SubnetUtilsMixin:
+    """
+    Mixin class to add useful utilities to IPv4Network and IPv6Network.
+    """
 
-    def divide(self, parts):
-        """ Generator to return the resulting subnets after dividing a main network """
+    def divide(
+        self, parts: int
+    ) -> t.Generator[t.Union[IPv4Network, IPv6Network], None, None]:
+        """Generator to return the resulting subnets after dividing a main network"""
 
         # Can't divide a subnet into less parts than 1, so guard against silliness
         if parts <= 1:
@@ -26,26 +35,65 @@ class SubnetUtilsMixin:
         else:
             subnet_diff = int(math.log2(parts)) + 1
 
-        if (subnet_diff+self.prefixlen) > self.max_prefixlen:
-            raise ValueError('Unable to divide %s into %s subnets.' % (self, parts))
+        if (subnet_diff + self.prefixlen) > self.max_prefixlen:
+            raise ValueError(f"Unable to divide {self} into {parts} subnets.")
 
         yield from self.subnets(prefixlen_diff=subnet_diff)
 
     __truediv__ = divide
 
-    def random_ip(self):
+    def random_ip(self) -> t.Union[IPv4Address, IPv6Address]:
         """Returns a random IP from this subnet"""
 
         random_address = randint(
-            int(self.network_address)+1,
-            int(self.broadcast_address)-1)
+            int(self.network_address) + 1, int(self.broadcast_address) - 1
+        )
 
         return builtin_ip_address(random_address)
 
 
 class IPv4Network(builtin_IPv4Network, SubnetUtilsMixin):
-    pass
+    """
+    This class represents and manipulates 32-bit IPv4 network + addresses..
+
+    Attributes: [examples for IPv4Network('192.0.2.0/27')]
+        .network_address: IPv4Address('192.0.2.0')
+        .hostmask: IPv4Address('0.0.0.31')
+        .broadcast_address: IPv4Address('192.0.2.32')
+        .netmask: IPv4Address('255.255.255.224')
+        .prefixlen: 27
+    """
+
+    pass  # pylint: disable=unnecessary-pass
 
 
 class IPv6Network(builtin_IPv6Network, SubnetUtilsMixin):
-    pass
+    """
+    This class represents and manipulates 128-bit IPv6 networks.
+
+    Attributes: [examples for IPv6('2001:db8::1000/124')]
+        .network_address: IPv6Address('2001:db8::1000')
+        .hostmask: IPv6Address('::f')
+        .broadcast_address: IPv6Address('2001:db8::100f')
+        .netmask: IPv6Address('ffff:ffff:ffff:ffff:ffff:ffff:ffff:fff0')
+        .prefixlen: 124
+    """
+
+    pass  # pylint: disable=unnecessary-pass
+
+
+def ip_network(*args, **kwargs) -> t.Union[IPv4Network, IPv6Network]:
+    """Create an IPv4 or IPv6 network object."""
+
+    net = builtin_ip_network(*args, **kwargs)
+
+    if isinstance(net, builtin_IPv4Network):
+        net.__class__ = IPv4Network
+
+    elif isinstance(net, builtin_IPv6Network):
+        net.__class__ = IPv6Network
+
+    else:
+        raise ValueError(f"Unhandled network type: {type(net)}")
+
+    return net
